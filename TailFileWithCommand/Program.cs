4cfcb7e0064c0b9e9fc886b96cc39f768856f27b
Dávid Kováč <daviz79@live.com>
2017-08-07 19:22:20 +0200

    Added basic tail functionality

--- TailFileWithCommand/Program.cs
+++ TailFileWithCommand/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using static System.Console;
@@ -12,8 +15,70 @@ namespace TailFileWithCommand
     {
         static void Main(string[] args)
         {
-            WriteLine("Hello");
-            WriteLine("World");
+            int exitCode = MainAsync(args).GetAwaiter().GetResult();
+
+            Environment.Exit(exitCode);
+        }
+
+        static async Task<int> MainAsync(string[] args)
+        {
+            if (args.Length < 2)
+                return 1;
+            string tailFilePath = args[0];
+            string commandPath = args[1];
+            string cmdArgs = string.Empty;
+            foreach (var a in args.Skip(2))
+                cmdArgs += a + " ";
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            var tailTask = TailFile(tailFilePath, cts.Token);
+
+            var proc = Process.Start(commandPath, cmdArgs);
+            var procTask = Task.Factory.StartNew(() =>
+            {
+                proc.WaitForExit();
+                cts.Cancel();
+            });
+
+            await Task.WhenAll(tailTask, procTask);
+
+            return proc.ExitCode;
+        }
+
+        public static async Task TailFile(string filePath, CancellationToken cancellationToken)
+        {
+            while(!File.Exists(filePath))
+            {
+                await Task.Delay(1000);
+                cancellationToken.ThrowIfCancellationRequested();
+            }
+            using (StreamReader reader = new StreamReader(new FileStream(filePath,
+                     FileMode.Open, FileAccess.Read, FileShare.ReadWrite)))
+            {
+                //start at the end of the file
+                long lastMaxOffset = 0;
+
+                while (true)
+                {
+                    await Task.Delay(100);
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    //if the file size has not changed, idle
+                    if (reader.BaseStream.Length == lastMaxOffset)
+                        continue;
+
+                    //seek to the last max offset
+                    reader.BaseStream.Seek(lastMaxOffset, SeekOrigin.Begin);
+
+                    //read out of the file until the EOF
+                    string line = "";
+                    while ((line = reader.ReadLine()) != null)
+                        WriteLine(line);
+
+                    //update the last max offset
+                    lastMaxOffset = reader.BaseStream.Position;
+                }
+            }
         }
     }
 }
